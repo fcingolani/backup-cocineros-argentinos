15ac1026d3ecd5db532c49151dcd259f60e6c6f3
Federico Cingolani <mail@fcingolani.com.ar>
2024-03-28 23:44:27 -0300

    Actualización de márgenes

--- scripts/build.js
+++ scripts/build.js
@@ -9,60 +9,82 @@ const recipes = JSON.parse(fs.readFileSync(`./${exportDir}/recipes.json`));
 const publicDir = './public';
 
 const indexTemplate = ejs.compile(`
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
-/>
-<div class="container">
-    <h1>Backup de recetas de Cocineros Argentinos</h1>
-    <% Object.keys(recipes).sort().forEach(function (category){ %>
-        <details>
-            <summary><%= category == 'undefined' ? "Otros" : category %></summary>
-            <ul>
-                <% recipes[category].sort((a, b) => a.title < b.title ? -1 : 1).forEach(function (recipe){ if(recipe){ %>
-                <li>
-                    <a href="<%= recipe.sourceURL.split('/').pop() %>.html"><%= recipe.title.toUpperCase() %></a> <% if(recipe.videoURL){ %>📺<% } %>
-                </li>
-                <% }}); %>
-            </ul>
-        </details>
+<!DOCTYPE html><html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Backup de Cocineros Argentinos</title>
+    <meta name="format-detection" content="telephone=no">
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+    />
+</head>
+<body>
+    <div class="container" style="padding-top: 1rem">
+        <h1>Backup de recetas de Cocineros Argentinos</h1>
+
         <hr />
-    <% }); %>
-    <p style="text-align: center;">Scrapeado con ❤️ por <a href="https://fcingolani.com.ar">@fcingolani</a>.</p>
-</div>
+        <% Object.keys(recipes).sort().forEach(function (category){ %>
+            <details>
+                <summary><%= category == 'undefined' ? "Otros" : category %></summary>
+                <ul>
+                    <% recipes[category].sort((a, b) => a.title < b.title ? -1 : 1).forEach(function (recipe){ if(recipe){ %>
+                    <li>
+                        <a href="<%= recipe.sourceURL.split('/').pop() %>.html"><%= recipe.title.toUpperCase() %></a> <% if(recipe.videoURL){ %>📺<% } %>
+                    </li>
+                    <% }}); %>
+                </ul>
+            </details>
+            <hr />
+        <% }); %>
+
+        <p style="text-align: center;">Scrapeado con ❤️ por <a href="https://fcingolani.com.ar">@fcingolani</a>.</p>
+    </div>
+</body>
 `);
 
 const recipeTemplate = ejs.compile(`
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
-/>
-<div class="container">
-    <p><a href="index.html">Ir al inicio</a></p>
-    <hr />
-    <h1>Backup de recetas de Cocineros Argentinos</h1>
-    <h2><%= recipe.title %></h2>
+<!DOCTYPE html><html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><%= recipe.title %> - Backup de Cocineros Argentinos</title>
+    <meta name="format-detection" content="telephone=no">
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+    />
+</head>
+<body>
+    <div class="container" style="padding-top: 1rem">
+        <h1>Backup de recetas de Cocineros Argentinos</h1>
+        <a href="index.html">Ir al inicio</a>
+        <hr />
 
-    <% if(recipe.videoURL){ %>
-        <p>
-            <iframe style="width: 100%; height: 50%;" src="<%= recipe.videoURL.replace("watch?v=", "embed/") %>"></iframe>
-        </p>
-    <% } %>
+        <h2><%= recipe.title %></h2>
 
-    <div class="grid">
-        <div>
-            <%- recipe.ingredients %>
+        <% if(recipe.videoURL){ %>
+            <p>
+                <iframe style="width: 100%; height: 50%;" src="<%= recipe.videoURL.replace("watch?v=", "embed/") %>"></iframe>
+            </p>
+        <% } %>
+
+        <div class="grid">
+            <div>
+                <%- recipe.ingredients %>
+            </div>
+            <div style="background-image: url('images/<%= recipe.imageFile %>'); background-size: contain; background-position: top right;">
+            
+            </div>
         </div>
-        <div style="background-image: url('images/<%= recipe.imageFile %>'); background-size: contain; background-position: top right;">
         
-        </div>
-    </div>
-    
-    <%- recipe.description %>
+        <%- recipe.description %>
 
-    <hr />
-    <p style="text-align: center;">Scrapeado con ❤️ por <a href="https://fcingolani.com.ar">@fcingolani</a>.</p>
-</div>
+        <hr />
+        <p style="text-align: center;">Scrapeado con ❤️ por <a href="https://fcingolani.com.ar">@fcingolani</a>.</p>
+    </div>
+</body>
 `);
 
 async function generateIndex(recipes, filePath){
